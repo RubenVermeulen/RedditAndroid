5a9576ceb9a99352291dae9e73413da6290e5a37
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-12-25 14:11:56 +0100

    Initializes project

---
+++ app/src/main/java/be/rubenvermeulen/android/redditapp/DataContext.java
@@ -0,0 +1,14 @@
+package be.rubenvermeulen.android.redditapp;
+
+public class DataContext {
+    public static String[] subreddits = {
+            "AskReddit",
+            "funny",
+            "todaylearned",
+            "science",
+            "pics",
+            "worldnews",
+            "announcements",
+            "gaming"
+    };
+}
